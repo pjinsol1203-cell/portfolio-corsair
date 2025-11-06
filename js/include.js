5df45617b69e11fca2e290268a03b468443eecec
jinsol <json_1203@naver.com>
2025-11-06 18:10:10 +0900

    header+footer 통합 / case-fan page 과정 /

--- js/include.js
+++ js/include.js
@@ -1,5 +1,9 @@
-document.addEventListener("DOMContentLoaded",()=>{
-
+fetch('/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector(".header-wrap").innerHTML += data
+        // 성공 시 이벤트 추가
+        
     const headerMenuLiArray = document.querySelectorAll(".header-menu>li")
     const gnbContainer = document.querySelector("#gnb-container")
     const gnbContainerSectionArray = document.querySelectorAll("#gnb-container>section")
@@ -16,4 +20,17 @@ document.addEventListener("DOMContentLoaded",()=>{
         gnbContainer.classList.remove("on")
         gnbContainerSectionArray.forEach(section=>section.classList.remove("on"))
     })
-})
+
+        })
+    .catch(error => console.log("에러 : ",error))
+
+    
+
+
+
+    fetch('/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector(".footer-wrap").innerHTML += data
+    })
+    .catch(error => console.log("에러 : ",error))
